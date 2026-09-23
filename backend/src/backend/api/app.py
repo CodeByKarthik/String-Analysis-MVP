bb9aee68e6fea7a14a8f5130af8bf9bf4e536436
Karthikeyan Sivakumar <126179797+Karthi-DStech@users.noreply.github.com>
2026-09-23 08:22:34 +0100

    Updated prometheus to app

--- backend/src/backend/api/app.py
+++ backend/src/backend/api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from backend.api.middleware import RequestResponseLoggingMiddleware
 from backend.api.routes.analysis import router as analysis_router
@@ -11,6 +12,10 @@ logger = get_logger(__name__)
 
 app = FastAPI(title="String Analysis Backend", version=settings.app_version)
 app.add_middleware(RequestResponseLoggingMiddleware)
+
+Instrumentator().instrument(app).expose(
+    app, endpoint="/metrics", include_in_schema=False
+)
 
 app.include_router(health_router)
 app.include_router(analysis_router)
